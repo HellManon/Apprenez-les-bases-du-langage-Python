2469efb69b1fa9495a0a83ebd09f71afeec7cb98
HellManon <manon.hell@outlook.fr>
2024-11-11 17:14:17 +0100

    MAJ exercice 3

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,16 @@
-# Écrivez votre code ici !
+# Définissez une variable  nom  contenant votre nom sous forme de chaîne de caractères.
+nom="toujours moi"
+
+#Définissez une variable  age  contenant votre âge sous forme d'un entier.
+age=35
+#Définissez une variable  taille  contenant votre taille sous forme d'un nombre à virgule flottante.
+taille=1.68
+#Définissez une variable  est_etudiant  contenant une valeur booléenne et indiquant si vous êtes étudiant ou non.
+est_etudiant=False
+#Utilisez la fonction print pour afficher les valeurs des variables précédemment définies.
+print(f"{nom} {age} {taille} {est_etudiant}")
+#Utilisez la fonction type pour afficher les types des variables précédemment définies Écrites
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est_etudiant))

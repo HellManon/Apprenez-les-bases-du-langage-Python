618eddcdc1d6ed4e3a9d8f6014f54f254d0d8f4f
HellManon <manon.hell@outlook.fr>
2024-11-11 16:36:27 +0100

    MAJ exercice 2

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+# Créez une variable nommée  nom
+nom = "moi"
+
+#Créez une variable nommée  age  
+age = 35
+
+#Affichez une phrase contenant votre nom et votre âge 
+print(f"Je m'appel {nom} et j'ai {age} ans.")
+
+#Modifiez la valeur de la variable  age  pour ajouter 10 ans à votre âge actuel
+age = age+10
+
+#Affichez à nouveau la phrase contenant votre nom et votre âge en utilisant les variables mises à jour
+print(f"Je m'appel {nom} et j'ai {age} ans.")

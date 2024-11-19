fc9d573329a3739b091b8c525f2079651da5db48
HellManon <manon.hell@outlook.fr>
2024-11-19 01:10:01 +0100

    MAJ exercice 1 partie 3

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,5 +1,12 @@
 # Ecrivez votre code ici !
+import operations
+def main():
 
+    resultat_addition = operations.addition(3 ,5)
+    print(resultat_addition)
+    
+    resultat_multiplication = operations.multiplication(8, 2)
+    print(resultat_multiplication)
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
